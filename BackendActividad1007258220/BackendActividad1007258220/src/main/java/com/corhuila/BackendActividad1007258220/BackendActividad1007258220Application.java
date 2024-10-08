22ccdeee4854d3db062ab6568f722032d1b1bba2
sparyock <55572135+sparyock@users.noreply.github.com>
2024-10-07 21:50:39 -0500

    Add files via upload

---
+++ BackendActividad1007258220/BackendActividad1007258220/src/main/java/com/corhuila/BackendActividad1007258220/BackendActividad1007258220Application.java
@@ -0,0 +1,13 @@
+package com.corhuila.BackendActividad1007258220;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendActividad1007258220Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendActividad1007258220Application.class, args);
+	}
+
+}
---
+++ BackendActividad1007258220/BackendActividad1007258220/src/test/java/com/corhuila/BackendActividad1007258220/BackendActividad1007258220ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corhuila.BackendActividad1007258220;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendActividad1007258220ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
